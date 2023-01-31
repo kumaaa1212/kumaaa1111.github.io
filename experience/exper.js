163a77ee83e94a68e876388dccab6426d20a7fd9
Ryohei <harrry.kuma1225@gmail.com>
2023-01-31 13:43:13 +0900

    mian

--- experience/exper.js
+++ experience/exper.js
@@ -2,7 +2,6 @@
 {
   {
     const dts = document.querySelectorAll('dt');
-    console.log(dts);
     dts.forEach(dt => {
       dt.addEventListener('click', () => {
         dt.parentNode.classList.toggle('appear');
