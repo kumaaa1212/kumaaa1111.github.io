5dd38129ebd4a85e3048ff0952b94e0dc5286ab6
Ryohei <harrry.kuma1225@gmail.com>
2023-01-31 12:33:05 +0900

    main

--- common.js
+++ common.js
@@ -27,13 +27,14 @@ window.onload = mainimg.src = image[2];
 }
 {
   const menu = document.getElementById('headmenu');
-  const showmenu = document.getElementById('showmenu')
+  const showmenu = document.getElementById('showmenu');
   menu.addEventListener('click', () =>{
     showmenu.classList.add('show');
     menu.classList.add('hidden');
   })
   showmenu.addEventListener('click', () =>{
     showmenu.classList.remove('show');
+  
     menu.classList.remove('hidden');
   })
 }
